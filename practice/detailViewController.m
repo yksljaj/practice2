d05cf4a2217958d5f65025081555169c7491b9df
Jessie <jessie@2017de-Mac-mini.local>
2018-02-22 23:34:31 +0800

    cannot double tap to zoom to original level

--- practice/detailViewController.m
+++ practice/detailViewController.m
@@ -7,8 +7,7 @@
 //
 
 #import "detailViewController.h"
-#define WIDTH_PAGE 414
-#define HEIGHT_PAGE 623
+
 @interface detailViewController ()
 {
     NSMutableArray *posterURL;
@@ -19,25 +18,18 @@
     CGFloat statusBarHeight;
     CGFloat navH;
     CGFloat tabH;
+    int pageNum;
+    int pageTag;
+    int scrollflag;
+    float pageOffset;
+    int prevPage;
 }
 @end
 
 @implementation detailViewController
 
-//- (id)initWithCoder:(NSCoder *)aDecoder{
-//    self = [super initWithCoder:aDecoder];
-//
-//    if (self) {
-//        NSString *nibName = NSStringFromClass([self class]);
-//        UINib *nib = [UINib nibWithNibName:detailViewController bundle:nil];
-//        [nib instantiateWithOwner:self options:nil];
-//        //Add the view loaded from the nib into self.
-//        [self addSubview:self.view];
-//    }
-//    return self;
-//}
 
--(void)viewWillAppear:(BOOL)animated{
+-(void)viewDidAppear:(BOOL)animated{
     NSLog(@"w:%f",self.view.frame.size.width);
     NSLog(@"h:%f",self.view.frame.size.height);
     
@@ -58,18 +50,21 @@
         [[UIDevice currentDevice] orientation] == UIDeviceOrientationPortraitUpsideDown || [[UIDevice currentDevice] orientation] == UIDeviceOrientationUnknown || [[UIDevice currentDevice] orientation] == UIDeviceOrientationFaceUp)
     {
         if((current_origin.x)!=(self.current_row*self.view.frame.size.width)&&((current_origin.x)!=0)){
-            position=CGPointMake((current_origin.x), 0);
+            position=CGPointMake((current_origin.x), -(statusBarHeight+navH));
         }else{
-            position = CGPointMake(self.view.frame.size.width*self.current_row, 0);
+            position = CGPointMake(self.view.frame.size.width*self.current_row, -(statusBarHeight+navH));
+           // [self createPinchScrollView];
         }
     }else{
-        if((current_origin.x)!=(self.current_row*667)&&((current_origin.x)!=0)){
-            position=CGPointMake((current_origin.x), 0);
+        if((current_origin.x)!=(self.current_row*self.view.frame.size.width)&&((current_origin.x)!=0)){
+            position=CGPointMake((current_origin.x), -(statusBarHeight+navH));
         }else{
-            position = CGPointMake(667*(self.current_row), 0);
+            position = CGPointMake(self.view.frame.size.width*(self.current_row),-(statusBarHeight+navH));
         }
     }
     [detailScrollView setContentOffset:position animated:NO];
+    NSLog(@"start pos: %.2f", position.x);
+    
     
 }
 - (void)viewDidLoad {
@@ -104,43 +99,43 @@
     
     [posterURL addObjectsFromArray:arrImgUrl];
     
-    for(int j=0;j<0;j++){
-        
-        [posterURL addObjectsFromArray:arrImgUrl];
-        
-    }
+    //    for(int j=0;j<1;j++){
+    //
+    //        [posterURL addObjectsFromArray:arrImgUrl];
+    //
+    //    }
     
     NSLog(@"poster count: %ld", (unsigned long)posterURL.count);
     
     
     
     
-//    //implement slide and pinch
-//    for(int i = 0; i < 20; i++){
-//        UITapGestureRecognizer *doubleTap =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleDoubleTap:)];
-//        [doubleTap setNumberOfTapsRequired:2];
-//
-//        pinchScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(375*20,0,375, 554)];
-//        pinchScrollView.delegate =self;
-//        pinchScrollView.showsVerticalScrollIndicator=NO;
-//        pinchScrollView.showsHorizontalScrollIndicator=NO;
-//        pinchScrollView.contentSize =CGSizeMake(375,554);
-//        pinchScrollView.minimumZoomScale =0.5;
-//        pinchScrollView.maximumZoomScale =5.0;
-//        [pinchScrollView setZoomScale:1.0];
-//
-//        imageview = [[UIImageView alloc]initWithFrame:CGRectMake(0,0,375, 554)];
-//        NSString *imageName = [posterURL objectAtIndex:i];
-//        imageview.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageName]]];
-//
-//
-//        imageview.userInteractionEnabled =YES;
-//        imageview.tag = i+1;
-//        [imageview addGestureRecognizer:doubleTap];
-//        [pinchScrollView addSubview:imageview];
-//        [self.detailScrollView addSubview:pinchScrollView];
-//        zoomflag=0;
-//    }
+    //    //implement slide and pinch
+    //    for(int i = 0; i < 20; i++){
+    //        UITapGestureRecognizer *doubleTap =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleDoubleTap:)];
+    //        [doubleTap setNumberOfTapsRequired:2];
+    //
+    //        pinchScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(375*20,0,375, 554)];
+    //        pinchScrollView.delegate =self;
+    //        pinchScrollView.showsVerticalScrollIndicator=NO;
+    //        pinchScrollView.showsHorizontalScrollIndicator=NO;
+    //        pinchScrollView.contentSize =CGSizeMake(375,554);
+    //        pinchScrollView.minimumZoomScale =0.5;
+    //        pinchScrollView.maximumZoomScale =5.0;
+    //        [pinchScrollView setZoomScale:1.0];
+    //
+    //        imageview = [[UIImageView alloc]initWithFrame:CGRectMake(0,0,375, 554)];
+    //        NSString *imageName = [posterURL objectAtIndex:i];
+    //        imageview.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageName]]];
+    //
+    //
+    //        imageview.userInteractionEnabled =YES;
+    //        imageview.tag = i+1;
+    //        [imageview addGestureRecognizer:doubleTap];
+    //        [pinchScrollView addSubview:imageview];
+    //        [self.detailScrollView addSubview:pinchScrollView];
+    //        zoomflag=0;
+    //    }
     
     zoomflag=0;
     
@@ -149,66 +144,79 @@
 
 
 -(void)scrollViewDidScroll:(UIScrollView *)_scrollView {
-    const int MAX_SEARCH_INVALID_VIEW_REGION =1000;
-    float pageOffset = ((float)_scrollView.contentOffset.x/(float)_scrollView.frame.size.width);
-    float p=_scrollView.frame.size.width;
-    float h=_scrollView.frame.size.height;
-    int pageNum = (_scrollView.contentOffset.x/_scrollView.frame.size.width);
-    NSLog(@"scrollview contentoffset x:%f",_scrollView.contentOffset.x);
-    NSLog(@"scrollview framesize width:%f",_scrollView.frame.size.width);
     
-    if((pageOffset - pageNum >FLT_EPSILON)
-         && ((pageOffset - pageNum) <0.5f))
-        pageNum++;
-   int pageTag = pageNum +1;
-    //NSLog(@"viewIsScrolling, pageNum: %d",pageNum);
-
-    NSLog(@"page offset: %.2f, scrollview position: %.2f _scrollView.tagID: %ld", pageOffset, _scrollView.contentOffset.x, (long)_scrollView.tag);
+    const int MAX_SEARCH_INVALID_VIEW_REGION =1000;
+    pageOffset = ((float)_scrollView.contentOffset.x/(float)_scrollView.frame.size.width);
+    pageNum = (_scrollView.contentOffset.x/_scrollView.frame.size.width);
+    
     if(_scrollView.tag!=0) {
         return;
     }
-    if([_scrollView viewWithTag:(pageTag)] ) {
-    NSLog(@"ZZ: view tag #%3d exist",pageTag);
+    NSLog(@"page offset: %.2f, scrollview position: %.2f _scrollView.tagID: %ld %d %d", pageOffset, _scrollView.contentOffset.x, (long)_scrollView.tag,pageNum,prevPage);
+    
+    if((pageOffset - pageNum >FLT_EPSILON)
+       && ((pageOffset - pageNum) <0.5f)){
+        pageNum++;
+        //NSLog(@"viewIsScrolling, pageNum: %d",pageNum);
+        
+        
+    }else{
+        
+    }
+    prevPage=pageNum;
+    
+    pageTag = pageNum +1;
+    
+    
+    if([_scrollView viewWithTag:(pageTag)] || pageNum>=posterURL.count) {
+        NSLog(@"ZZ: view tag #%3d exist",pageTag);
         return;
     }
     else{
         // view is missing, create it and set its tag to currentPage+1
         NSLog(@"ZZ: view tag #%3d ismissing, need to create that", pageTag);
-#if 1
-        UITapGestureRecognizer *doubleTap =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleDoubleTap:)];
-        [doubleTap setNumberOfTapsRequired:2];
-        UIScrollView *tmpPinchView = [[UIScrollView alloc]initWithFrame:
-                                           CGRectMake((pageNum)*_scrollView.frame.size.width,
-                                                                     0,
-                                                                     _scrollView.frame.size.width,
-                                                                     _scrollView.frame.size.height)];
-        //CGSize size=tmpPinchView;
-        tmpPinchView.delegate =self;
-        tmpPinchView.showsVerticalScrollIndicator=NO;
-        tmpPinchView.showsHorizontalScrollIndicator=NO;
-        tmpPinchView.contentSize =CGSizeMake(self.view.frame.size.width,self.view.frame.size.height-statusBarHeight-navH-tabH);
-        tmpPinchView.minimumZoomScale =0.5;
-        tmpPinchView.maximumZoomScale =5.0;
-        [tmpPinchView setZoomScale:1.0];
-        
-        UIImageView *tmpImgView = [[UIImageView alloc]initWithFrame:CGRectMake(0,0,self.view.frame.size.width, self.view.frame.size.height-statusBarHeight-navH-tabH)];
-        NSString *imageName = [posterURL objectAtIndex:pageNum];
-        tmpImgView.image =[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageName]]];
-      
-        tmpImgView.userInteractionEnabled =YES;
-        [tmpImgView addGestureRecognizer:doubleTap];
-        [tmpPinchView addSubview:tmpImgView];
-        tmpPinchView.tag =pageTag;
-        [_scrollView addSubview:tmpPinchView];
-#endif
+        [self createPinchScrollView:_scrollView atPage:pageNum];
     }
-   
+    
     for (int i=0; i < MAX_SEARCH_INVALID_VIEW_REGION; i++ ) {
         if((i < (pageNum-1) || i > (pageNum+1)) && [_scrollView viewWithTag:(i+1)] ) {
             [[_scrollView viewWithTag:(i+1)] removeFromSuperview];
             NSLog(@"ZZ: \t\t\t\tdelete view tag #%3d", i+1);
         }
     }
+    
+}
+
+-(void)createPinchScrollView:(UIScrollView *)_srcScrollView atPage:(int)pageNumber {
+    
+    UIScrollView *tmpPinchView;
+    UIImageView *tmpImgView;
+    tmpPinchView = [[UIScrollView alloc]initWithFrame:
+                    CGRectMake((pageNumber)*_srcScrollView.frame.size.width,
+                               0,
+                               _srcScrollView.frame.size.width,
+                               _srcScrollView.frame.size.height)];
+    //CGSize size=tmpPinchView;
+    tmpPinchView.delegate =self;
+    tmpPinchView.showsVerticalScrollIndicator=NO;
+    tmpPinchView.showsHorizontalScrollIndicator=NO;
+    tmpPinchView.contentSize =CGSizeMake(self.view.frame.size.width,self.view.frame.size.height-statusBarHeight-navH-tabH);
+    tmpPinchView.minimumZoomScale =0.5;
+    tmpPinchView.maximumZoomScale =5.0;
+    [tmpPinchView setZoomScale:1.0];
+    
+    tmpImgView = [[UIImageView alloc]initWithFrame:CGRectMake(0,0,self.view.frame.size.width, self.view.frame.size.height-statusBarHeight-navH-tabH)];
+    NSString *imageName = [posterURL objectAtIndex:pageNumber];
+    tmpImgView.image =[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageName]]];
+    
+    tmpImgView.userInteractionEnabled =YES;
+    
+    UITapGestureRecognizer *doubleTap =[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleDoubleTap:)];
+    [doubleTap setNumberOfTapsRequired:2];
+    [tmpImgView addGestureRecognizer:doubleTap];
+    [tmpPinchView addSubview:tmpImgView];
+    tmpPinchView.tag =pageNumber+1;
+    [_srcScrollView addSubview:tmpPinchView];
 }
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
@@ -218,63 +226,90 @@
     }
     return nil;
 }
+//- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+//
+//    NSLog(@"scrollViewDidEndDragging");
+//    if (decelerate) {
+//
+//
+//        for (UIScrollView *s in scrollView.subviews){
+//            if ([s isKindOfClass:[UIScrollView class]]){
+//                [s setZoomScale:1.0 animated:YES]; //scrollView每滑动一次将要出现的图片较正常时候图片的倍数（将要出现的图片显示的倍数）
+//            }
+//        }
+//    }else{
+//        NSLog(@"no decelerate");
+//
+//    }
+//
+//    CGPoint point=scrollView.contentOffset;
+//    NSLog(@"%f,%f",point.x,point.y);
+//
+//}
 
--(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
-    
-    if (scrollView ==detailScrollView){
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)_scrollView{
+    float currentPage = _scrollView.contentOffset.x /_scrollView.frame.size.width;
+    bool isDiffPage = false;
+    if(_scrollView == detailScrollView)
+    {
         
-        CGFloat x = scrollView.contentOffset.x;
-        if (x==-300){
-            
-        }
-        else {
-            //            offset = x;
-            for (UIScrollView *s in scrollView.subviews){
+        if(currentPage != prevPage)
+        {
+            isDiffPage = true;
+            for (UIScrollView *s in _scrollView.subviews){
                 if ([s isKindOfClass:[UIScrollView class]]){
                     [s setZoomScale:1.0]; //scrollView每滑动一次将要出现的图片较正常时候图片的倍数（将要出现的图片显示的倍数）
                 }
             }
         }
-    }
-}
-
-
--(void)handleDoubleTap:(UIGestureRecognizer *)gesture{
-    
-    if(zoomflag==0){
-        float enlarge = [(UIScrollView*)gesture.view.superview zoomScale] * 3.0;//每次双击放大倍数
-        CGRect zoomRect = [self zoomRectForScale:enlarge withCenter:[gesture locationInView:gesture.view]];
-        [(UIScrollView*)gesture.view.superview zoomToRect:zoomRect animated:YES];
-        zoomflag=1;
-    }else{
-        [(UIScrollView*)gesture.view.superview setZoomScale:1.0 animated:YES];
+        
         zoomflag=0;
+        NSLog(@"end dec page: %.2f, isDiffPage: %d", currentPage, isDiffPage);
     }
 }
-
-- (CGRect)zoomRectForScale:(float)scale withCenter:(CGPoint)center{
-    CGRect zoomRect;
-    zoomRect.size.height =self.view.frame.size.height / scale;
-    zoomRect.size.width  =self.view.frame.size.width  / scale;
-    zoomRect.origin.x = center.x - (zoomRect.size.width  /2.0);
-    zoomRect.origin.y = center.y - (zoomRect.size.height /2.0);
-    return zoomRect;
     
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
-
-@end
+    
+    
+    -(void)handleDoubleTap:(UIGestureRecognizer *)gesture{
+        
+        if(zoomflag==0){
+            NSLog(@"%f",self.view.frame.size.height);
+            NSLog(@"%f",self.view.frame.size.width);
+            
+            float enlarge = [(UIScrollView*)gesture.view.superview zoomScale] * 5.0;//每次双击放大倍数
+            CGRect zoomRect = [self zoomRectForScale:enlarge withCenter:[gesture locationInView:gesture.view]];
+            [(UIScrollView*)gesture.view.superview zoomToRect:zoomRect animated:YES];
+            zoomflag=1;
+        }else{
+            [(UIScrollView*)gesture.view.superview setZoomScale:1.0 animated:YES];
+            zoomflag=0;
+        }
+    }
+    
+    - (CGRect)zoomRectForScale:(float)scale withCenter:(CGPoint)center{
+        CGRect zoomRect;
+        
+        zoomRect.size.height =self.view.frame.size.height / scale;
+        zoomRect.size.width  =self.view.frame.size.width  / scale;
+        zoomRect.origin.x = center.x - (zoomRect.size.width  /2.0);
+        zoomRect.origin.y = center.y - (zoomRect.size.height /2.0);
+        return zoomRect;
+        
+    }
+    
+    - (void)didReceiveMemoryWarning {
+        [super didReceiveMemoryWarning];
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     #pragma mark - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @end
